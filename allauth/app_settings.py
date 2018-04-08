9c3919051cbd11fc03617575f020d32b1b5501ff
Tobias Göcke <tobiasgoecke@users.noreply.github.com>
2018-04-09 00:01:43 +0200

    adding

--- allauth/app_settings.py
+++ allauth/app_settings.py
@@ -6,3 +6,6 @@ SOCIALACCOUNT_ENABLED = 'allauth.socialaccount' in settings.INSTALLED_APPS
 LOGIN_REDIRECT_URL = getattr(settings, 'LOGIN_REDIRECT_URL', '/')
 
 USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+
+
+ACCOUNT_REGISTRATION_OPEN = getattr(settings, 'ACCOUNT_REGISTRATION_OPEN', 'True')
